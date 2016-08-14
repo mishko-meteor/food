6b5f6cf03032da2608b65974e6edc1586f3a4acc
Mishko <mishko.meteor@gmail.com>
2016-08-14 18:05:09 +0300

    'server'

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
+var port = "process.env.PORT || 3030"
 
 app.use(express.static(__dirname + '/'));
 
 
-app.listen(3030, function () {
-    console.log('app is up and running at port 3030');
+app.listen(port, function () {
+    console.log('app is up and running at port ' + port);
 });
