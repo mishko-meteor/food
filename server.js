ece648ceec01e1dc68df42ffa5b5de42d0c464df
Mishko <mishko.meteor@gmail.com>
2016-08-14 18:18:04 +0300

    'bbbw'

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var port = "process.env.PORT || 3000"
 
 app.use(express.static(__dirname + '/'));
 
+app.get('/', function (req, res) { 
+    res.sendFile('index.html');
+});
+
 
 app.listen(port, function () {
     console.log('app is up and running at port ' + port);
