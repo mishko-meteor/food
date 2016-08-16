da6f8cb01d5b8c612bbef631f33e7f109e46f714
Mishko <mishko.meteor@gmail.com>
2016-08-16 10:26:21 +0300

    'blaj'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,7 +7,7 @@ app.controller('mainCtrl', ['$scope', '$firebaseObject', '$timeout','$interval',
     
     var now = new Date();
     $scope.today = now.getDate() + '.' + (now.getMonth()+1) + '.' + now.getFullYear() + '_' + now.getHours() + '-' + now.getMinutes();
-    $scope.todayForDisplay = now.getDate() + '.' + (now.getMonth()+1) + '.' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes();
+    
     $scope.deleteAllLabel = 'לחצו למחיקת כלל הנתונים';
     $scope.currentView = 'home';
 
@@ -20,21 +20,6 @@ app.controller('mainCtrl', ['$scope', '$firebaseObject', '$timeout','$interval',
     var errorMessage = error.message;
     // ...
     });
-
-    // $timeout(function(){
-    //     var d = angular.element(document.querySelector('#fader'))
-    //     console.log(d);
-    //     // var d = document.getElementById("fader");
-    //     d.removeClass("hidden");
-    //     d.addClass("fadeInUp");
-    // },1000)
-    // // authanticate anonymously into firebase db
-    // firebase.auth().signInAnonymously().catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     console.log('error: ' + errorMessage);
-    // });
 
     // get data after authentication 
     firebase.auth().onAuthStateChanged(function(user) {
@@ -57,6 +42,25 @@ app.controller('mainCtrl', ['$scope', '$firebaseObject', '$timeout','$interval',
             // User is signed out.
         }
     });
+    
+    $scope.startTime = function () {
+        var now = new Date();
+        var h = now.getHours();
+        var m = now.getMinutes();
+        var s = now.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        $scope.todayForDisplay = now.getDate() + '.' + (now.getMonth()+1) + '.' + now.getFullYear() + ' ' + h + ":" + m + ":" + s;
+        $timeout($scope.startTime, 1000);
+    }
+    
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
+    $scope.startTime();
+
 
     $scope.increase = function(enterance) {
         $scope.data[enterance].counter++;
